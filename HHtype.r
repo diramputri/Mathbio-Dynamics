9b6d1f490d5cec4cf20884eed69834281d8e7bd0
Andira Putri <35618440+diramputri@users.noreply.github.com>
2019-03-20 20:50:58 -0400

    fixed equations for V and n

--- HHtype.r
+++ HHtype.r
@@ -1,8 +1,8 @@
 #Hodgkin-Huxley type model
 
 #installing and calling the necessary toolkits
-install.packages(deSolve)
-install.packages("phaseR")
+#install.packages("deSolve")
+#install.packages("phaseR")
 #once you've installed the two packages, you can comment out the above part
 library(deSolve)
 library(phaseR)
@@ -15,34 +15,35 @@ HHmodel <- function(t,y,parameters) {
   I <- parameters[2]
   gL <- parameters[3]
   gNa <- parameters[4]
-  mm <- parameters[5]
+  v12a <- parameters[5]
   EL <- parameters[6]
-  nn <- parameters[7]
+  v12b <- parameters[7]
   ENa <- parameters[8]
   gk <- parameters[9]
   Ek <- parameters[10]
   tau <- parameters[11]
+  k1 <- parameters[12]
+  k2 <- parameters[13]
   dy <- numeric(2)
-  dy[1] <- ((I-gL*(V-EL)-gNa*mm*V*(V-ENa))-n*(gk*(V-Ek)))/C
-  dy[2] <- (nn-n)/tau
+  dy[1] <- ((I-gL*(V-EL)-gNa*(1/1+exp((v12a-V/k1)))*V*(V-ENa))-n*(gk*(V-Ek)))/C
+  dy[2] <- ((1/1+exp((v12b-V/k2)))-n)/tau
   list(dy)
 }
 
-#vector field (helps with plotting trajectories)
 #there's an argument for parameters...parameters = c(...)
 #this is where we define the parameter values
-#c(V,n,C,I,gL,gNa,mm,EL,nn,ENa,gk,Ek,tau)
+#c(C,I,gL,gNa,v12a,EL,v12b,nn,ENa,gk,Ek,tau,k1,k2)
 
-HHmodel.flowfield <- flowField(HHmodel,xlim = c(-100, 100),ylim = c(-50, 50),
-                parameters = c(1,0,8,20,1,-80,1,60,10,90,1),points = 19,add = FALSE,
-                main = "Neuron Firing Dynamics",xlab="V",ylab="n")
+HHmodel.flowfield <- flowField(HHmodel,xlim = c(-100, 100),ylim = c(-200, 50),
+                parameters = c(1,0,8,20,-20,-80,-25,60,10,-90,1,15,5),points = 20,add = FALSE,
+                main = "Scenario A",xlab="membrane voltage, V",ylab="K+ activation variable, n")
 
 #adding a grid to our plot
 grid()
 
 #plotting nullclines
-HHmodel.nullclines <- nullclines(HHmodel, xlim = c(-100, 100), ylim = c(-50, 50),
-                 parameters = c(1,0,8,20,1,-80,1,60,10,90,1), points = 500,lwd=2,
+HHmodel.nullclines <- nullclines(HHmodel, xlim = c(-100, 100), ylim = c(-200, 50),
+                 parameters = c(1,0,8,20,-20,-80,-25,60,10,-90,1,15,5), points = 500,lwd=2,
                  col=c("red","blue"))
 
 #starting points for trajectories
