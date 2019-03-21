0b29972bca49703f42d0407031e3297ae77f8d93
Andira Putri <35618440+diramputri@users.noreply.github.com>
2019-03-20 20:51:49 -0400

    fixed V & n equations; resolved convergence issues

--- HHmodel.py
+++ HHmodel.py
@@ -5,20 +5,22 @@ import antimony
 r = te.loada ('''
     model pathway()
 
-  $X -> V; ((I-gL*(V-EL)-gNa*mm*V*(V-ENa))-n*(gk*(V-Ek)))/C
-  $X -> n; (nn-n)/tau
+  $X -> V; ((I-gL*(V-EL)-gNa*(1/1+exp((v12a-V/k1)))*V*(V-ENa))-n*(gk*(V-Ek)))/C
+  $X -> n; ((1/1+exp((v12b-V/k2)))-n)/tau
 
 C=1
 I=0
 gL=8
 gNa=20
-mm=1
 EL=-80
-nn=1
 ENa=60
 gk=10
 Ek=90
 tau=1
+v12a=-20
+k1=15
+v12b=-25
+k2=5
 
 end''')
 
